66a6cad2683a0f6962c5fa5a6260c2ca7bc50bf0
Fernando César Martins <fernandoark@gmail.com>
2017-01-21 19:58:02 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+/*
+if ($_SERVER['HTTP_HOST'] == "localhost")
+{
+    // BANCO DE DADOS LOCAL
+    define("DB_SERVER", "localhost");
+    define("DB_USERNAME", "root");
+    define("DB_PASSWORD", "");
+    define("DB_NAME", "tccagendaonline_tcc");
+}
+else
+{*/
+    // BANCO DE DADOS NO SERVIDOR
+    define("DB_SERVER", "localhost");
+    define("DB_USERNAME", "root");
+    define("DB_PASSWORD", "");
+    define("DB_NAME", "tccagendaonline_tcc");
+//}
+
+// CONFIGURACOES DO SITE
+define("NOME_DO_SITE", "Agenda Escolar Online");
+define("URL_SITE","http://www.tccagendaonline.com.br");
+
+?>
